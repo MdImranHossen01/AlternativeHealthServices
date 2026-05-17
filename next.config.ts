f1fbaa7a64ffe8b437dbed552f3cd750f456d0cb
MdImranHossen01 <imranshuvo101@gmail.com>
2026-05-17 07:08:29 +0600

    improve performance

--- next.config.ts
+++ next.config.ts
@@ -36,7 +36,7 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 60,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    qualities: [75, 80],
+    qualities: [50, 60, 75, 80, 90, 100],
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
